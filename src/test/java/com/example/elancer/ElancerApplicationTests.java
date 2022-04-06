ca8bf83d50498c7fd4343ed92c1b84e17bfc0885
mins1031 <urisegea@naver.com>
2022-04-06 13:33:07 +0900

    feat: 첫 커밋

---
+++ src/test/java/com/example/elancer/ElancerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.elancer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElancerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
