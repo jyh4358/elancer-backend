5ffcc5476c0c52737dbbb601014abc1403eb66a6
mins1031 <urisegea@naver.com>
2022-04-23 23:10:18 +0900

    feat: position 레포지토리 구현 완료

--- src/main/java/com/example/elancer/freelancerprofile/repository/position/planner/PlannerRepository.java
+++ src/main/java/com/example/elancer/freelancerprofile/repository/position/planner/PlannerRepository.java
@@ -1,7 +1,7 @@
 package com.example.elancer.freelancerprofile.repository.position.planner;
 
-import com.example.elancer.freelancerprofile.model.position.CrowdWorker;
+import com.example.elancer.freelancerprofile.model.position.planner.Planner;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PlannerRepository extends JpaRepository<Plan> {
+public interface PlannerRepository extends JpaRepository<Planner, Long> {
 }
