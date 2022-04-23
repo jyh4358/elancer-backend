e787d03169e6a77d27877f9899723ab2f3e59350
mins1031 <urisegea@naver.com>
2022-04-23 23:10:18 +0900

    feat: position 레포지토리 구현 완료

---
+++ src/main/java/com/example/elancer/freelancerprofile/repository/position/planner/PlanFieldRepository.java
@@ -0,0 +1,7 @@
+package com.example.elancer.freelancerprofile.repository.position.planner;
+
+import com.example.elancer.freelancerprofile.model.position.planner.PlanField;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlanFieldRepository extends JpaRepository<PlanField, Long> {
+}
