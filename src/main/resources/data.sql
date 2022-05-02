2f96f0e4cbeba2f39a571dd3879bcccbd172748f
jyh4358 <jyh4358@gmail.com>
2022-05-02 15:09:36 +0900

    feat: sql 기본 데이터

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,36 +1,36 @@
 --  사업 분야
-INSERT INTO `elancer`.`main_business` (`main_code`, `biz_name`) VALUES ('sub_biz1', '웹개발');
-INSERT INTO `elancer`.`main_business` (`main_code`, `biz_name`) VALUES ('sub_biz2', '앱개발');
-INSERT INTO `elancer`.`main_business` (`main_code`, `biz_name`) VALUES ('sub_biz3', '솔루션개발');
-INSERT INTO `elancer`.`main_business` (`main_code`, `biz_name`) VALUES ('sub_biz4', 'GIS개발');
-INSERT INTO `elancer`.`main_business` (`main_code`, `biz_name`) VALUES ('sub_biz5', 'POS개발');
-INSERT INTO `elancer`.`main_business` (`main_code`, `biz_name`) VALUES ('sub_biz6', 'SI사업');
-INSERT INTO `elancer`.`main_business` (`main_code`, `biz_name`) VALUES ('sub_biz7', '기타');
+INSERT INTO `elancer`.`main_business` (`main_code`, `biz_name`) VALUES ('main_biz1', '웹개발');
+INSERT INTO `elancer`.`main_business` (`main_code`, `biz_name`) VALUES ('main_biz2', '앱개발');
+INSERT INTO `elancer`.`main_business` (`main_code`, `biz_name`) VALUES ('main_biz3', '솔루션개발');
+INSERT INTO `elancer`.`main_business` (`main_code`, `biz_name`) VALUES ('main_biz4', 'GIS개발');
+INSERT INTO `elancer`.`main_business` (`main_code`, `biz_name`) VALUES ('main_biz5', 'POS개발');
+INSERT INTO `elancer`.`main_business` (`main_code`, `biz_name`) VALUES ('main_biz6', 'SI사업');
+INSERT INTO `elancer`.`main_business` (`main_code`, `biz_name`) VALUES ('main_biz7', '기타');
 
 --  업무 분야
-INSERT INTO `elancer`.`sub_business` (`sub_code`, `biz_name`) VALUES ('main_biz1', '쇼핑몰');
-INSERT INTO `elancer`.`sub_business` (`sub_code`, `biz_name`) VALUES ('main_biz2', '여행사');
-INSERT INTO `elancer`.`sub_business` (`sub_code`, `biz_name`) VALUES ('main_biz3', '금융');
-INSERT INTO `elancer`.`sub_business` (`sub_code`, `biz_name`) VALUES ('main_biz4', '증권');
-INSERT INTO `elancer`.`sub_business` (`sub_code`, `biz_name`) VALUES ('main_biz5', '카드');
-INSERT INTO `elancer`.`sub_business` (`sub_code`, `biz_name`) VALUES ('main_biz6', '보험');
-INSERT INTO `elancer`.`sub_business` (`sub_code`, `biz_name`) VALUES ('main_biz7', '병원');
-INSERT INTO `elancer`.`sub_business` (`sub_code`, `biz_name`) VALUES ('main_biz8', '대학');
-INSERT INTO `elancer`.`sub_business` (`sub_code`, `biz_name`) VALUES ('main_biz9', '공공기관');
-INSERT INTO `elancer`.`sub_business` (`sub_code`, `biz_name`) VALUES ('main_biz10', '물류');
-INSERT INTO `elancer`.`sub_business` (`sub_code`, `biz_name`) VALUES ('main_biz11', '회계');
-INSERT INTO `elancer`.`sub_business` (`sub_code`, `biz_name`) VALUES ('main_biz12', '제조');
-INSERT INTO `elancer`.`sub_business` (`sub_code`, `biz_name`) VALUES ('main_biz13', '건설');
-INSERT INTO `elancer`.`sub_business` (`sub_code`, `biz_name`) VALUES ('main_biz14', '통신');
-INSERT INTO `elancer`.`sub_business` (`sub_code`, `biz_name`) VALUES ('main_biz15', '유통');
-INSERT INTO `elancer`.`sub_business` (`sub_code`, `biz_name`) VALUES ('main_biz16', '생산');
-INSERT INTO `elancer`.`sub_business` (`sub_code`, `biz_name`) VALUES ('main_biz17', '미디어');
-INSERT INTO `elancer`.`sub_business` (`sub_code`, `biz_name`) VALUES ('main_biz18', '교육');
-INSERT INTO `elancer`.`sub_business` (`sub_code`, `biz_name`) VALUES ('main_biz19', '반도체');
-INSERT INTO `elancer`.`sub_business` (`sub_code`, `biz_name`) VALUES ('main_biz20', '자동차');
-INSERT INTO `elancer`.`sub_business` (`sub_code`, `biz_name`) VALUES ('main_biz21', '암호화페');
-INSERT INTO `elancer`.`sub_business` (`sub_code`, `biz_name`) VALUES ('main_biz22', '블록체인');
-INSERT INTO `elancer`.`sub_business` (`sub_code`, `biz_name`) VALUES ('main_biz23', '기타');
+INSERT INTO `elancer`.`sub_business` (`sub_code`, `biz_name`) VALUES ('sub_biz1', '쇼핑몰');
+INSERT INTO `elancer`.`sub_business` (`sub_code`, `biz_name`) VALUES ('sub_biz2', '여행사');
+INSERT INTO `elancer`.`sub_business` (`sub_code`, `biz_name`) VALUES ('sub_biz3', '금융');
+INSERT INTO `elancer`.`sub_business` (`sub_code`, `biz_name`) VALUES ('sub_biz4', '증권');
+INSERT INTO `elancer`.`sub_business` (`sub_code`, `biz_name`) VALUES ('sub_biz5', '카드');
+INSERT INTO `elancer`.`sub_business` (`sub_code`, `biz_name`) VALUES ('sub_biz6', '보험');
+INSERT INTO `elancer`.`sub_business` (`sub_code`, `biz_name`) VALUES ('sub_biz7', '병원');
+INSERT INTO `elancer`.`sub_business` (`sub_code`, `biz_name`) VALUES ('sub_biz8', '대학');
+INSERT INTO `elancer`.`sub_business` (`sub_code`, `biz_name`) VALUES ('sub_biz9', '공공기관');
+INSERT INTO `elancer`.`sub_business` (`sub_code`, `biz_name`) VALUES ('sub_biz10', '물류');
+INSERT INTO `elancer`.`sub_business` (`sub_code`, `biz_name`) VALUES ('sub_biz11', '회계');
+INSERT INTO `elancer`.`sub_business` (`sub_code`, `biz_name`) VALUES ('sub_biz12', '제조');
+INSERT INTO `elancer`.`sub_business` (`sub_code`, `biz_name`) VALUES ('sub_biz13', '건설');
+INSERT INTO `elancer`.`sub_business` (`sub_code`, `biz_name`) VALUES ('sub_biz14', '통신');
+INSERT INTO `elancer`.`sub_business` (`sub_code`, `biz_name`) VALUES ('sub_biz15', '유통');
+INSERT INTO `elancer`.`sub_business` (`sub_code`, `biz_name`) VALUES ('sub_biz16', '생산');
+INSERT INTO `elancer`.`sub_business` (`sub_code`, `biz_name`) VALUES ('sub_biz17', '미디어');
+INSERT INTO `elancer`.`sub_business` (`sub_code`, `biz_name`) VALUES ('sub_biz18', '교육');
+INSERT INTO `elancer`.`sub_business` (`sub_code`, `biz_name`) VALUES ('sub_biz19', '반도체');
+INSERT INTO `elancer`.`sub_business` (`sub_code`, `biz_name`) VALUES ('sub_biz20', '자동차');
+INSERT INTO `elancer`.`sub_business` (`sub_code`, `biz_name`) VALUES ('sub_biz21', '암호화페');
+INSERT INTO `elancer`.`sub_business` (`sub_code`, `biz_name`) VALUES ('sub_biz22', '블록체인');
+INSERT INTO `elancer`.`sub_business` (`sub_code`, `biz_name`) VALUES ('sub_biz23', '기타');
 
 
 
