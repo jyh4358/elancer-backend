061feeaba277d8863c09541ff4a297190900fb63
mins1031 <urisegea@naver.com>
2022-07-20 20:42:41 +0900

    feat: 프로젝트 찜 삭제 로직 변경

--- src/main/java/com/example/elancer/wishprojects/repository/WishProjectRepository.java
+++ src/main/java/com/example/elancer/wishprojects/repository/WishProjectRepository.java
@@ -4,7 +4,10 @@ import com.example.elancer.wishprojects.model.WishProject;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface WishProjectRepository extends JpaRepository<WishProject, Long> {
     List<WishProject> findByFreelancerNum(Long freelancerNum);
+
+    Optional<WishProject> findByFreelancerNumAndProjectNum(Long freelancerNum, Long projectNum);
 }
