062f13550620588f3d9ec795fb2fd759c59d5c45
jyh4358 <jyh4358@gmail.com>
2022-07-21 19:22:05 +0900

    feat: 프로젝트 기업로고 로직 추가

---
+++ src/main/java/com/example/elancer/enterprise/repository/EnterpriseLogoRepository.java
@@ -0,0 +1,7 @@
+package com.example.elancer.enterprise.repository;
+
+import com.example.elancer.project.model.EnterpriseLogo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EnterpriseLogoRepository extends JpaRepository<EnterpriseLogo, Long> {
+}
